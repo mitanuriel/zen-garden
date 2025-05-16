da5e8737461a78666dc0d2310618df69d3643f5e
mitanuriel <145558996+mitanuriel@users.noreply.github.com>
2025-05-17 01:28:00 +0200

    Created src, navigation and file for AuthStack. Wrapped ex View into Navigationcontainer, then used NAvigator, created Screens with components AuthStack and MainStack

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useEffect } from 'react'; 
-import * as SplashScreen from 'expo-splash-screen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-SplashScreen.preventAutoHideAsync();
+
+const {Navigator, Screen} = createNativeStackNavigator();
 
 export default function App() {
-  useEffect(() => {
-    setTimeout(() => SplashScreen.hideAsync(), 2000);
-  }, []);
-
+ 
   return (
-    <View style={styles.container}>
-      <Text>Welcome to Zen Garden</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Navigator>
+        <Screen name='AuthStack' component={AuthStack} />
+        <Screen name='MainStack' component={AppStack} />
+      
+      
+      </Navigator>
+      </NavigationContainer>
+    
   );
 }
 
---
+++ src/navigation/AuthStack.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const AuthStack = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text>AuthStack</Text>
+    </View>
+  );
+};
+
+export default AuthStack;
+
+const styles = StyleSheet.create({
+  container: {},
+});
